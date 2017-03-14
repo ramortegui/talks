1dc520e160cad0f9407bd33b1d1fe2b38ff84093
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-03-13 22:05:16 -0600

    Add sample code (factorial and txt file.)

---
+++ elixir-basics-2/Factorial.ex
@@ -0,0 +1,12 @@
+defmodule Factorial do
+  def of(num) when num >=0 do
+    _of(num,1)
+  end
+
+  defp _of(num, acc) when num == 0 do
+    acc
+  end
+  defp _of(num, acc) when num > 1 do
+    _of(num-1, acc*num)
+  end
+end
