1330a591eba8e02a7935676bdf4ea74124240656
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-05-02 18:39:02 -0600

    Document calculator script.

--- elixir-otp/lib/calculator_script.exs
+++ elixir-otp/lib/calculator_script.exs
@@ -1,3 +1,23 @@
+#Start the calculator with zero
 val = 0;
-sum = fn(val,add) -> val + add end
-sub = fn(val,sub) -> val - sub end
+
+#Define an anonymous function for add
+sum = fn(val,add) -> 
+  val + add 
+end
+
+#Define an anonymous function for sub
+sub = fn(val,sub) -> 
+  val - sub 
+end
+
+IO.puts "Inital value #{val}"
+
+IO.puts "Add 1"
+sum.(val,1)
+
+IO.puts "After add 1: #{val} !wrong"
+
+val = sum.(val,1)
+
+IO.puts "After add 1: #{val}";
