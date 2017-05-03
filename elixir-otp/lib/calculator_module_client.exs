eba58cb0ac0860c555d8b9f4e40f6bb20ec7ed1b
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-05-02 21:36:44 -0600

    Add calculator module documentation.

--- elixir-otp/lib/calculator_module_client.exs
+++ elixir-otp/lib/calculator_module_client.exs
@@ -1,18 +1,19 @@
+#Load Module
 import_file("calculator.ex")
-
+#create a process
 pid = Calculator.init(10)
-
+#add 1
 send pid, {:+, 1}
-
+#create function to receive messages
 receive_message = fn() ->
   receive do
     {:ok, val} -> IO.puts val
     after 3000 -> IO.puts "No messages"
   end
 end
-
+#get the pid
 mypid = self()
-
+#ask for results
 send pid,{:=, mypid}
-
+#print results
 receive_message.()
