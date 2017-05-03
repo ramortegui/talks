365c6008c9852345aeb62baa7e19cc1420f8a8eb
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-05-02 21:37:20 -0600

    Add initial value to the application.

--- elixir-otp/lib/calculator_application.ex
+++ elixir-otp/lib/calculator_application.ex
@@ -1,9 +1,10 @@
+#Module using Application behaviour
 defmodule CalculatorApplication do
   use Application
   def start(_type_,_other_) do
     import Supervisor.Spec, warn: false
     children = [
-       supervisor(CalculatorSupervisor, [])
+       supervisor(CalculatorSupervisor, [10])
        ]
       opts = [strategy: :one_for_one, name: __MODULE__]
       Supervisor.start_link(children, opts)
